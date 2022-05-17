14e0699daebf220755632c0586c1f2d0bb1b7b45
Walden4u <walden4u@gmail.com>
2022-05-17 22:14:15 +0900

    5_1

---
+++ components/Greet.js
@@ -0,0 +1,5 @@
+import React, { component } from 'react';
+
+const Greet = () => <h1>Hello Grace!</h1>;
+
+export default Greet;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from "./App";
+// import App from "./App";
+import App from '../components/Greet';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
