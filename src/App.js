ede9ee9d38ba6ac8d1612dbdf726f24a48b2cc34
Walden4u <walden4u@gmail.com>
2022-05-18 06:51:44 +0900

    6_1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
+import Greet from "../components/Greet";
+import GreetClass from "../components/GreetClass";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Greet />
+      <GreetClass />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-// import App from "./App";
-import App from "../components/GreetClass";
+import App from "./App";
+// import App from "../components/GreetClass";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
