f09d0ee964d1b7f2921de463f36c8196c0cbf46e
Walden4u <walden4u@gmail.com>
2022-05-18 22:53:44 +0900

    8_1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import Greet from "../components/Greet";
 import GreetClass from "../components/GreetClass";
+import Hello from "../components/Hello";
 
 export default function App() {
   return (
     <div className="App">
-      <Greet />
-      <GreetClass />
+      {/* <Greet />
+      <GreetClass /> */}
+      <Hello />
     </div>
   );
 }
